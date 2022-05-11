7fbc42ab2e97fe8dda4c04b8aab35c8060d13a4d
jiaLEE <jia9510@naver.com>
2022-05-11 17:30:17 +0900

    test 02

---
+++ src/test/Test02.java
@@ -0,0 +1,14 @@
+package test;
+
+public class Test02 {
+
+	public static void main(String[] args) {
+		// TODO study Algorithm
+		int a = 0;
+		int b = 5;
+		System.out.println(a + b);
+		
+
+	}
+
+}
